262701696ad5d2c288dff2f15e2b8de61cb49362
agustingom99 <agustingom99@gmail.com>
2022-08-17 13:05:05 -0300

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './Components/app/app-routing.module';
+import { AppComponent } from './Components/app/app.component';
+import { HeaderComponent } from './Components/header/header.component';
 
+/* Componentes & pipes*/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
+
+  /* Modulos */
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  /* servicios*/
   providers: [],
+  /* Componente principal*/
   bootstrap: [AppComponent]
 })
 export class AppModule { }
