213b9bd7383ec28f52af260a0aff91d8d431c9fe
agustingom99 <agustingom99@gmail.com>
2022-08-19 12:56:58 -0300

    eliminar este commit luego

--- src/app/Components/app/app.component.ts
+++ src/app/Components/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'my-app';
+  usuario = [];
+
+  constructor( private _http: HttpClient){};
+
+  ngOnInit() {
+    this._http.get('https://jsonplaceholder.typicode.com/users')
+    .subscribe((datos : any) => this.usuario = datos);
+
+   
+
+  }
+
 }
