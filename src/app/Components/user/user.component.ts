e1d9d3b9e4313afde18f57407d84b5d7c20d92d5
agustingom99 <agustingom99@gmail.com>
2022-08-19 12:57:25 -0300

    Eliminar este commit luego

--- src/app/Components/user/user.component.ts
+++ src/app/Components/user/user.component.ts
@@ -10,23 +10,7 @@ export class UserComponent  {
 
   constructor() { }
 
-  isLogged: boolean = false;
-  nivel: number =5;
-
-  user: Usuario = <Usuario>{
-    ID:23,
-    Nombre: "alex",
-    Apellido: "Pepe",
-    Email: "alex@gmail.com",
-    Nick: "alex",
-    Contraseña: "alex",
-    tipo: UserType.Administrador
   
-  }
-
-  mostrar(): void{
-    console.log(this.user.Apellido);
-  }
    
 
 }
